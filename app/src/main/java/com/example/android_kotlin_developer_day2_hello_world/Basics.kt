bdabdd601d140d30ca545a32f5b8132a6c3dcaca
Alex Castillo <alex.castillo@razortracking.com>
2025-02-06 13:05:23 -0600

    val and var using in hello world

--- app/src/main/java/com/example/android_kotlin_developer_day2_hello_world/Basics.kt
+++ app/src/main/java/com/example/android_kotlin_developer_day2_hello_world/Basics.kt
@@ -1,5 +1,9 @@
 package com.example.android_kotlin_developer_day2_hello_world
 
 fun main() {
-    println("Hello, World!")
+    var age = 5
+    age += 1
+    var name: String = "John"
+    val pi: Double = 3.14159 // immutable variable
+    println("Hello, $name! You are $age years old. Also, pi is $pi.")
 }
